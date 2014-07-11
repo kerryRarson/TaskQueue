96c144bd2ff1a687e63d2e20df7d3ee9cf0fd88d
kerryRarson <klarsen@w-link.net>
2014-07-11 07:51:04 -0600

    added logging

--- SAC.TaskQueue/Program.cs
+++ SAC.TaskQueue/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using log4net;
 using Ninject;
 using Ninject.Parameters;
 using SAC.TaskQueue.Data;
@@ -15,20 +16,37 @@ namespace SAC.TaskQueue
     {
         static void Main(string[] args)
         {
-            using (var ctx = new DataproDB())
-            {
-                var qItem = ctx.TaskQueue.Where(q => q.StatusId == 1).OrderBy(q => q.Id).FirstOrDefault();
-                if (qItem != null)
-                {
-                    //get the concrete implementation from the DI binding
-                    IKernel kernel = new StandardKernel();
-                    kernel.Load(Assembly.GetExecutingAssembly());
-                    var task = kernel.Get<ITaskQueue>(qItem.Task.BindingName, new ConstructorArgument("taskQueueId", qItem.Id));
+            //initialize the log
+            log4net.Config.XmlConfigurator.Configure();
+            ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Debug(String.Format("starting."));
 
-                    //run the task
-                    var result = task.Run(qItem.Id);
+            try
+            {
+
+
+                using (var ctx = new DataproDB())
+                {
+                    var qItem = ctx.TaskQueue.Where(q => q.StatusId == 1).OrderBy(q => q.Id).FirstOrDefault();
+                    if (qItem != null)
+                    {
+                        log.Debug(String.Format("running TaskQueue Id - '{0}'", qItem.Id));
+                        //get the concrete implementation from the DI binding
+                        IKernel kernel = new StandardKernel();
+                        kernel.Load(Assembly.GetExecutingAssembly());
+                        log.Debug(string.Format("getting binding {0} from ninject kernel", qItem.Task.BindingName));
+                        var task = kernel.Get<ITaskQueue>(qItem.Task.BindingName, new ConstructorArgument("taskQueueId", qItem.Id));
+                        log.Debug(String.Format("type '{0}' returned from kernel", task.GetType().Name));
+                        //run the task
+                        var result = task.Run(qItem.Id);
+                    }
                 }
             }
+            catch (Exception err)
+            {
+                log.Error(err.ToString());
+            }
+            log.Debug("Ending");
         }
     }
 }
